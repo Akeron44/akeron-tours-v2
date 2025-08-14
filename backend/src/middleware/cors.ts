24240295f94fcc1e8618242074b8993408f4d435
Akeron44 <oniakeron@gmail.com>
2025-08-14 16:19:07 +0200

    Updated allowed origins

--- backend/src/middleware/cors.ts
+++ backend/src/middleware/cors.ts
@@ -1,11 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
 
+const allowedOrigins = [
+  'http://localhost:8080',
+  'https://akeron-tours-v2.vercel.app',
+];
+
 export const corsMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  const origin = req.headers.origin!;
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  } else {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  }
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   next();
